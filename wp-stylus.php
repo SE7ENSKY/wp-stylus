b1c5b795aa2832a17ae14cf909bbdb89f03f3285
Ivan Kravchenko <i@se7ensky.com>
2013-10-30 21:37:53 +0200

    added stylus @import support, fixed cache location

--- wp-stylus.php
+++ wp-stylus.php
@@ -6,14 +6,12 @@
   Tags: stylus, css, stylesheet
   Author: Se7enSky studio
   Author URI: http://github.com/Se7enSky
-  Version: 1.1
+  Version: 1.2
   License: The MIT License
   License file: LICENSE
  */
 
 namespace se7ensky\stylus;
-
-define('CACHE', __DIR__ . '/cache');
 
 function pipeRender($url, $source) {
 	$ch = curl_init();
@@ -29,6 +27,12 @@ function pipeRender($url, $source) {
 	return $output;
 }
 
+function handleImports($source, $originalFile) {
+	return preg_replace_callback('/@import\s+\'(?<import>.*)\'/', function($a) use ($originalFile) {
+		return file_get_contents(dirname($originalFile) . '/' . $a['import'] . '.styl');
+	}, $source);
+}
+
 function stylus2css($source) {
 	return pipeRender('http://render.se7ensky.com/stylus', $source);
 }
@@ -38,11 +42,12 @@ function host() {
 	if (preg_match('/^(.*)\.css$/', $uri, $matches)) {
 		$name = $matches[1];
 		$srcFile = __DIR__ . '/../../..' . $name . '.styl';
-		$cachedFile = CACHE . $uri;
+		$cachedFile = __DIR__ . '/cache' . $uri;
 		if (file_exists($srcFile)) {
 			if (!file_exists($cachedFile) || filemtime($cachedFile) < filemtime($srcFile)) {
 				@mkdir(dirname($cachedFile), 0770, true);
 				$source = file_get_contents($srcFile);
+				$source = handleImports($source, $srcFile);
 				$compiled = stylus2css($source);
 				file_put_contents($cachedFile, $compiled);
 			}
